c53eeb9db7668c44b4cff6169e8c78db1b6aa311
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-12-18 05:39:14 +0200

    Added Unit Of Work Pattern

---
+++ FoodDeliverySystem/DAL/EF/EFUnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using Catalog.DAL.Repositories.Impl;
+using Catalog.DAL.Repositories.Interfaces;
+using Catalog.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.DAL.EF
+{
+    public class EFUnitOfWork
+        : IUnitOfWork
+    {
+        private readonly FoodDeliverySystemContext dbContext;
+        private IOrderRepository orderRepository;
+        private IClientRepository clientRepository;
+        private IUserRepository userRepository;
+
+        public EFUnitOfWork(DbContextOptions options)
+        {
+            dbContext = new FoodDeliverySystemContext(options);
+        }
+
+        public IOrderRepository Orders
+        {
+            get
+            {
+                if (orderRepository == null)
+                    orderRepository = new OrderRepository(dbContext);
+                return orderRepository;
+            }
+        }
+
+        public IClientRepository Clients
+        {
+            get
+            {
+                if (clientRepository == null)
+                    clientRepository = new ClientRepository(dbContext);
+                return clientRepository;
+            }
+        }
+
+        public IUserRepository Users
+        {
+            get
+            {
+                if (userRepository == null)
+                    userRepository = new UserRepository(dbContext);
+                return userRepository;
+            }
+        }
+
+        public void Save()
+        {
+            dbContext.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    dbContext.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ FoodDeliverySystem/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.DAL.Repositories.Interfaces;
+
+namespace Catalog.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IOrderRepository Orders { get; }
+        IClientRepository Clients { get; }
+        IUserRepository Users { get; }
+        void Save();
+    }
+}
