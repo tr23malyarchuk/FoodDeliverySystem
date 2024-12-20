18af44ab91e2bafa3543064527cf5eea970066cb
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-12-21 01:56:43 +0200

    Test class and Moq package were added to the project

---
+++ FoodDeliverySystem/DAL.Tests/TestOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.DAL.Entities;
+using Catalog.DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+class TestOrderRepository
+ : BaseRepository<Order>
+{
+    public TestOrderRepository(DbContext context)
+        : base(context)
+    {
+    }
+}
