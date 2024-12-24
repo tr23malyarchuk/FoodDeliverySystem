5f6816b0b409f5783901fd0f8ca00f339df2b2aa
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-12-24 17:09:02 +0200

    Implementing end-to-end functionality

---
+++ FoodDeliverySystem/CCL/Security/SecurityContext.cs
@@ -0,0 +1,15 @@
+﻿namespace OSBB.Security
+{
+    public static class SecurityContext
+    {
+        static User _user = null;
+        public static User GetUser()
+        {
+            return _user;
+        }
+        public static void SetUser(User user)
+        {
+            _user = user;
+        }
+    }
+}
